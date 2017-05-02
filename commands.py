737d790ec7062afae6ab87b1801ff7593ffaf5a8
Raelian Movement <irwin@DESKTOP-FHAENV2.(none)>
2017-05-02 03:31:06 -0500

    start_http.py

--- commands.py
+++ commands.py
@@ -23,7 +23,6 @@ def usage():
           "- u[pdate]: update cache",
           "- v[alidate] [sample_size=300]: validate up to sample_size cache"
              " items",
-          "- s[erve]: start serving http.",
           "- v[otes]: show your current vote data",
           "- r[eset_cache]: remove everything from the cache",
           "- n[aive_debug]: show some data on the naive bayes classifier",
@@ -58,17 +57,6 @@ if __name__ == '__main__':
             post_data.validate_cache(dataset, print_progress=True)
         else:
             post_data.validate_cache(dataset, sample_size, print_progress=True)
-    elif command in ['s', 'serve']:
-        server_address = ('127.0.0.1', 8000)
-        print("Serving on:",
-              f"http://{server_address[0]}:{server_address[1]}/")
-        try:
-            handler = http_server.RecommendationRequestHandler(server_address)
-            handler.server.serve_forever()
-        except KeyboardInterrupt:
-            pass
-        else:
-            exit(1) # Just in case .serve_forever() fails somehow.
     elif command in ['v', 'votes']:
         dataset = post_data.Dataset()
         print("Good:", dataset.good)
