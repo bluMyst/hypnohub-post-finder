79c8296ed7418c534a19bba09a95a01276abbeb6
bluMyst <weirdusername@gmail.com>
2017-05-02 19:39:04 -0500

    Removed test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import post_data
-
-if __name__ == '__main__':
-    # TODO: Will 32187 it still be missing from the cache after I reload the
-    # whole thing?
-    post_data.chunk_validate_cache(post_data.Dataset())
