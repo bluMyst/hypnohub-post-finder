526ece3e5109d5d2a9b27c9122bac90ced55f3c3
Ahto <weirdusername@gmail.com>
2017-05-23 11:31:20 -0500

    Cleaner commands.py

--- commands.py
+++ commands.py
@@ -35,65 +35,53 @@ of my code, for doing things that you can't yet do from HTTP.
 #   server. We might have to store a cookie to know who is who.
 
 
-def usage():
-    print("Usage:", sys.argv[0], "<command>")
-    print()
-    print("Possible commands:",
-          "- u[pdate]: update cache",
-
-          "- v[alidate] [sample_size=300]: validate up to sample_size cache"
-          " items",
-
-          "- r[eset_cache]: remove everything from the cache",
-
-          "- re[cord_votes] <user>: get the likes and favorites from <user>"
-          " and add them to dataset.good.",
-
-          "- c[heck_deleted]: check to see if any of the dataset id's have"
-          " been deleted.",
-          sep='\n')
-
-
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        usage()
-        exit(1)
-
-    command = sys.argv[1].lower()
-
-    if command in ['u', 'update']:
-        dataset = post_data.Dataset()
-        dataset.update_cache(print_progress=True)
-        dataset.save()
-    elif command in ['v', 'validate']:
-        dataset = post_data.Dataset()
-
+class CommandHandler(object):
+    def __call__(self, args):
         try:
-            sample_size = int(sys.argv[2])
+            script_name, command, *args = args
         except ValueError:
-            usage()
+            self.usage(args[0])
             exit(1)
-        except IndexError:
-            post_data.validate_cache(dataset, print_progress=True)
-        else:
-            post_data.validate_cache(dataset, sample_size, print_progress=True)
-    elif command in ['v', 'votes']:
-        dataset = post_data.Dataset()
-        print("Good:", dataset.good)
-        print("Bad:", dataset.bad)
-    elif command in ['r', 'reset_cache']:
-        dataset = post_data.Dataset()
 
+        command = command.lower()
+
+        if not hasattr(self, f'do_{command}'):
+            self.usage(script_name)
+            exit(1)
+
+        getattr(self, f'do_{command}')(args)
+
+    def usage(self, script_name):
+        print("Usage:", script_name, "<command>")
+        print()
+        print("Possible commands:")
+
+        for i in dir(self):
+            if i.startswith("do_"):
+                f = getattr(self, i)
+                print('-', f.__doc__)
+
+    @ahto_lib.lazy_property
+    def dataset(self):
+        return post_data.Dataset()
+
+    def do_update(self, args):
+        '''update: Update the Hypnohub cache.'''
+        self.dataset.update_cache(print_progress=True)
+        self.dataset.save()
+
+    def do_reset(self, args):
+        '''reset: Clear the Hypnohub cache.'''
         if ahto_lib.yes_no(False, "Reset cache? Are you sure? [yN]"):
             print("Erasing cache...")
-            dataset.cache = {}
-            dataset.save()
+            self.dataset.cache = {}
+            self.dataset.save()
         else:
             print("Your cache is safe!")
-    elif command in ['re', 'record_votes']:
-        dataset = post_data.Dataset()
 
-        user = sys.argv[2]
+    def do_record_votes(self, args):
+        '''record_votes <user>: Add a user's votes to the dataset.'''
+        user = args[0]
 
         if not ahto_lib.yes_no(None, f"Record votes for {user}?"):
             exit(0)
@@ -105,30 +93,33 @@ if __name__ == '__main__':
             good_ids |= hhapi.get_vote_data(sys.argv[2], 2)
 
         print("Got", len(good_ids), "items.")
-        print("Saving in cache...", end=' ')
-        dataset.good |= good_ids
-        dataset.save()
-        print("done.")
-    elif command in ['ch', 'check_deleted']:
-        with ahto_lib.LoadingDone("Loading dataset..."):
-            dataset = post_data.Dataset()
 
-        dataset.update_cache(print_progress=True)
+        with ahto_lib.LoadingDone("Saving in cache..."):
+            self.dataset.good |= good_ids
+            self.dataset.save()
 
-        for post_id in dataset.good | dataset.bad:
-            if dataset.get_id(post_id).deleted:
-                if ahto_lib.yes_no(False,
-                                   f"Post #{post_id} appears to be deleted."
-                                   f" Remove from dataset?"):
-                    dataset.good -= {post_id}
-                    dataset.bad  -= {post_id}
+    def do_check_deleted(self, args):
+        '''check_deleted: Check if any cached posts have been deleted.'''
+        self.dataset.update_cache(print_progress=True)
+
+        for post_id in self.dataset.good | self.dataset.bad:
+            if self.dataset.get_id(post_id).deleted:
+                yn = ahto_lib.yes_no(
+                    False,
+                    f"Post #{post_id} appears to be deleted."
+                    f" Remove from dataset?")
+
+                if yn:
+                    self. dataset.good -= {post_id}
+                    self.dataset.bad  -= {post_id}
                     print("Removed")
                 else:
                     print("Not removed.")
 
-        print("Saving...", end=' ')
-        dataset.save()
-        print("done.")
-    else:
-        usage()
-        exit(1)
+        with ahto_lib.LoadingDone('Saving...'):
+            self.dataset.save()
+
+
+if __name__ == '__main__':
+    ch = CommandHandler()
+    ch(sys.argv)
