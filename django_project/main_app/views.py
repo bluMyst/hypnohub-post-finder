c84ed41a1661e78ccafd504cf247ea420411aecb
bluMyst <weirdusername@gmail.com>
2018-02-20 19:26:02 -0600

    Added a site map / main menu kind of thing

--- django_project/main_app/views.py
+++ django_project/main_app/views.py
@@ -7,13 +7,32 @@ from django.shortcuts import render
 # Ctrl-C safe!
 # https://stackoverflow.com/questions/842557/how-to-prevent-a-block-of-code-from-being-interrupted-by-keyboardinterrupt-in-py
 
-def view_id(request, id_):
+def main_menu(request):
+    context = {
+        'links': [
+            ('view-a-best-post',   'View the best posts we can find for you..'),
+            ('view-a-hot-post',    'View a mix of good and bad posts, to help train'
+                                   ' the AI.'),
+            ('view-a-random-post', 'View posts completely at random, to teach the AI'
+                                   ' about your preferences.'),
+        ],
+
+        'title': "Site Map",
+    }
+
+    return render(template_name='main_app/path_index.html',
+                  request=request,
+                  context=context)
+
+def view_id(request, id_, **context):
     context = {
         'post_id': id_,
 
         # TODO: Retrieve preview url
         # TODO: Store Hypnohub data in the database.
-        'image_url': "http://hypnohub.net/data/image/2d3bec75d3c52b70f61e592250b22ed7.jpg"
+        'image_url': "http://hypnohub.net/data/image/2d3bec75d3c52b70f61e592250b22ed7.jpg",
+
+        **context
     }
 
     return render(template_name='main_app/post_view.html',
@@ -23,7 +42,10 @@ def view_id(request, id_):
 permalink = view_id
 
 def best(request):
-    return view_id(request, 1337)
+    return view_id(request, 1337, tabtitle="Best Posts")
 
-hot = best
-random = best
+def hot(request):
+    return view_id(request, 1337, tabtitle="Hot Posts")
+
+def random(request):
+    return view_id(request, 1337, tabtitle="Random Posts")
