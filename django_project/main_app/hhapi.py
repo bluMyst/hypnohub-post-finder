ba929ae68beb1b11a1cddccf44a9e317f0c9862d
bluMyst <weirdusername@gmail.com>
2018-03-03 02:27:29 -0600

    Fixed a ton of bugs in hhapi
    Also the documentation is more accurate and better-worded now

--- django_project/main_app/hhapi.py
+++ django_project/main_app/hhapi.py
@@ -6,6 +6,8 @@ import requests
 
 BASE_USERAGENT = "AhtoHypnohubCrawlerBot/0.0"
 USERAGENT = BASE_USERAGENT + " (mailto://weirdusername@techie.com)"
+
+# Be very polite to the server.
 DELAY_BETWEEN_REQUESTS = 2
 
 """
@@ -44,13 +46,19 @@ class HypnohubAPIRequester(object):
         if not robots_allowed:
             raise EnvironmentError("robots.txt disallowed us!")
 
-    def get_posts(self, tags=None, page=None, limit=None):
+    def get_posts(self, tags=None, page=None, limit=200):
         """
         Returns a list of dicts, parsed straight from the JSON. One for each
         post.
 
-        This will return a maximum of 100 posts. Can be decreased from there by
-        setting 'limit'.
+        You can set limit to 'None' to not send a limit. If you do this,
+        Hypnohub will probably end up sending you 30 posts at a time.
+
+        Apparently you can set 'limit' to absurdly high numbers (I've tried up
+        to 1000) and Hypnohub will happily churn along and give you the data
+        you want. With this great power comes great responsibility: Don't
+        overtax the servers! It's probably better to ask for little chunks of
+        data at a time. Like about 200 posts at a time?
 
         Remember that this will be in descending order, by id unless you ask
         for order:id or something like that. It works exactly like the
@@ -89,7 +97,7 @@ class HypnohubAPIRequester(object):
         Be careful with this method! Don't just load all of HypnoHub into RAM
         at once. This function is lazy for a reason.
         """
-        current_page = 0
+        current_page = 1
         posts = []
 
         while True:
@@ -105,25 +113,30 @@ class HypnohubAPIRequester(object):
         # id_desc is the default order but I'm setting it just to be safe.
         return self.get_posts('order:id_desc', limit=1)[0]['id']
 
-    def get_complete_post_list(self, callback=None):
+    def get_complete_post_list(self):
         """
-        The callback is called with chunks of 100 posts, as they come in.
-        Callback args: (new_posts, highest_id_seen, highest_id_in_hypnohub)
+        yields (new_posts, highest_id_seen, highest_id_in_hypnohub)
+
+        new_posts is a list of posts, usually 100 of them
 
         The last two args are for progress bars and such.
 
         It's completely fine to use get_all_matching_posts for the same purpose
-        as this function. The only difference is this one uses a callback
-        instead of 'yield'.
+        as this function. The only difference is this one yields values in a
+        different way, and won't let you set a 'tags' value.
         """
-        current_page    = 0
+        current_page    = 1
         highest_id_seen = None
 
         highest_id = self.get_highest_id()
 
         while True:
-            new_posts = self.get_posts("order:id", current_page)
+            new_posts = self.get_posts(tags="order:id", page=current_page)
+            current_page += 1
+
+            if len(new_posts) == 0:
+                break
+
             highest_id_seen = new_posts[-1]['id']
 
-            if callback is not None:
-                callback(new_posts, highest_id_seen, highest_id)
+            yield new_posts, highest_id_seen, highest_id
