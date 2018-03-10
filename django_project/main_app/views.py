7ec4cdc7c5e619496f53982d8c9dfa46d3e39fea
bluMyst <weirdusername@gmail.com>
2018-03-10 14:35:46 -0600

    Added crontabbing update_cache to design goals

--- django_project/main_app/views.py
+++ django_project/main_app/views.py
@@ -3,13 +3,6 @@ from django.http import Http404
 
 from .models import Post
 
-# TODO: Every time the user requests something that requires knowledge about
-# Post's, check what was the last time we've updated the database with fresh
-# Hypnohub data. If it's been more than - say - a week, update it in a separate
-# thread and set a flag so there's never more than one thread working. Make it
-# Ctrl-C safe!
-# https://stackoverflow.com/questions/842557/how-to-prevent-a-block-of-code-from-being-interrupted-by-keyboardinterrupt-in-py
-
 def main_menu(request):
     context = {
         'title': "Site Map",
